756ab3823b27b56497ee4b14423815bb2c7037c0
Markus Felten <Markus.Felten@gmx.de>
2017-10-08 21:46:42 +0200

    test: use public mqtt server

--- src/worker.js
+++ src/worker.js
@@ -1,33 +1,31 @@
 const mqtt = require('mqtt');
 
-//'mqtt://test.mosquitto.org'
+export async function worker(leveldb, paths, options) {
+  const client = mqtt.connect(options.url);
 
-function worker(options) {
-  const client = mqtt.connect('mqtt://test.mosquitto.org');
-
-  client.on('connect', () => {
-    client.subscribe('presence');
-    client.publish('presence', 'Hello mqtt');
-  });
-
-  client.on('message', (topic, message) => {
-    console.log(message.toString());
-
-    const inserts = [];
-
-    const date = '123456';
-    const amount = 1234.56;
-    const type = '/some/path';
-
-    const key = `${type}/${date.padStart(10, '0')}`;
-    inserts.push({ type: 'put', key, value: amount });
-
-    leveldb.batch(inserts, err => {
-      if (err) {
-        console.log(err);
-      }
+  return new Promise((resolve, reject) => {
+    client.on('connect', () => {
+      paths.forEach(p => client.subscribe(p));
+      resolve(client);
     });
 
-    //client.end();
+    client.on('message', (topic, message) => {
+      const data = JSON.parse(message.toString());
+
+      const inserts = [];
+
+      const date = new Date(data.date);
+      const amount = data.value;
+      const key = `${topic}/${date.getTime() /*.padStart(10, '0')*/}`;
+
+      inserts.push({ type: 'put', key, value: amount });
+
+      leveldb.batch(inserts, err => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(key);
+      });
+    });
   });
 }
--- tests/simple-test.js
+++ tests/simple-test.js
@@ -3,26 +3,60 @@ import { worker } from '../src/worker';
 
 const levelup = require('levelup');
 const path = require('path');
+const mqtt = require('mqtt');
+
+const MQTT_BROKER_URL = 'mqtt://test.mosquitto.org';
+const PATH_1 = 'mqtt-timeseries-leveldb/test1';
 
 test.cb('write', t => {
+  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const dates = [];
+
   t.plan(1);
 
   const leveldb = levelup(path.join(__dirname, '../build', 'leveldb'));
 
-  worker(leveldb, { url: 'mqtt://test.mosquitto.org' });
+  worker(leveldb, [PATH_1], { url: MQTT_BROKER_URL });
 
-  const readStream = leveldb.createReadStream({ start: 'pv/0', end: 'pv/Z' });
+  const client = mqtt.connect(MQTT_BROKER_URL);
 
-  readStream.on('data', data => {
-    //console.log(data.key + ' = ' + data.value);
+  client.on('connect', () => {
+    let count = 0;
 
-    if (data.key === 'pv/1030665600' && data.value == 2004.1) {
-      t.pass();
-    }
+    let d = new Date();
+    dates.push(d);
+
+    client.publish(PATH_1, JSON.stringify({ date: d, value: values[count++] }));
+    setTimeout(() => {
+      let d = new Date();
+      dates.push(d);
+      client.publish(
+        PATH_1,
+        JSON.stringify({ date: d, value: values[count++] })
+      );
+    }, 500);
   });
 
-  readStream.on('end', () => {
-    leveldb.close();
-    t.end();
-  });
+  setTimeout(() => {
+    const readStream = leveldb.createReadStream({
+      start: `${PATH_1}/0`,
+      end: `${PATH_1}/Z`
+    });
+
+    readStream.on('data', data => {
+      //console.log(data.key + ' = ' + data.value);
+      const t = parseInt(data.key.substring(PATH_1.length + 1), 10);
+      //console.log(t);
+      const date = new Date(t);
+      console.log(`${date} <> ${dates[0]} ${data.value} <> ${values[0]}`);
+      if (date == dates[0] && data.value == values[0]) {
+        t.pass();
+      }
+    });
+
+    readStream.on('end', () => {
+      leveldb.close();
+      t.end();
+    });
+  }, 3000);
 });
