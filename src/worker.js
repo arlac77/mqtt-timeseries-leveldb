6ab81b82f305d4ba16dc7e6666b804e75fb374ac
Markus Felten <Markus.Felten@gmx.de>
2017-12-10 09:25:54 +0100

    fix: use scalar put

--- src/worker.js
+++ src/worker.js
@@ -11,18 +11,13 @@ export async function worker(leveldb, paths, options) {
 
     client.on('message', (topic, message) => {
       const data = JSON.parse(message.toString());
-
-      const inserts = [];
-
       const date = new Date(data.date);
-      const amount = data.value;
       const key = `${topic}/${date.getTime() /*.padStart(10, '0')*/}`;
+      const value = data.value;
 
       //  console.log(`write ${key} = ${amount}`);
 
-      inserts.push({ type: 'put', key, value: amount });
-
-      leveldb.batch(inserts, err => {
+      leveldb.put(key, value, err => {
         if (err) {
           reject(err);
         }
--- tests/simple-test.js
+++ tests/simple-test.js
@@ -10,68 +10,65 @@ const MQTT_BROKER_URL = 'mqtt://test.mosquitto.org';
 const PATH_1 = 'mqtt-timeseries-leveldb/test1';
 
 test.cb('write', t => {
-  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  const dates = [];
-
   t.plan(13);
 
   const leveldb = levelup(
     leveldown(path.join(__dirname, '..', 'build', 'leveldb'))
   );
 
-  worker(leveldb, [PATH_1], { url: MQTT_BROKER_URL });
+  worker(leveldb, [PATH_1], { url: MQTT_BROKER_URL }).then(() => {
+    const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const dates = [];
 
-  const client = mqtt.connect(MQTT_BROKER_URL);
+    let matches = 0;
 
-  client.on('connect', () => {
-    let count = 0;
+    const client = mqtt.connect(MQTT_BROKER_URL);
 
-    setInterval(() => {
-      const d = new Date();
-      dates.push(d.getTime());
-      client.publish(
-        PATH_1,
-        JSON.stringify({ date: d, value: values[count++] })
-      );
-    }, 300);
-  });
+    client.on('connect', () => {
+      let count = 0;
 
-  let matches = 0;
+      let iv = setInterval(() => {
+        const d = new Date();
+        dates.push(d.getTime());
+        client.publish(
+          PATH_1,
+          JSON.stringify({ date: d, value: values[count++] })
+        );
 
-  setTimeout(() => {
-    const readStream = leveldb.createReadStream({
-      start: `${PATH_1}/0`,
-      end: `${PATH_1}/Z`
-    });
+        if (count === values.length) {
+          clearInterval(iv);
+          const readStream = leveldb.createReadStream({
+            start: `${PATH_1}/0`,
+            end: `${PATH_1}/Z`
+          });
 
-    readStream.on('data', data => {
-      //console.log(data.key + ' = ' + data.value);
-      const date = parseInt(
-        data.key.toString().substring(PATH_1.length + 1),
-        10
-      );
-      //console.log(data.key.toString() + ' = ' + data.value);
+          readStream.on('data', data => {
+            const date = parseInt(
+              data.key.toString().substring(PATH_1.length + 1),
+              10
+            );
 
-      const i = dates.indexOf(date);
+            const i = dates.indexOf(date);
 
-      if (i >= 0) {
-        //console.log(`${date} -> ${i}`);
+            if (i >= 0) {
+              t.is(parseInt(data.value.toString(), 10), values[i]);
 
-        t.is(parseInt(data.value.toString(), 10), values[i]);
+              if (parseInt(data.value.toString(), 10) == values[i]) {
+                //console.log(`${date} <> ${values[i]}`);
 
-        if (parseInt(data.value.toString(), 10) == values[i]) {
-          console.log(`${date} <> ${values[i]}`);
+                if (++matches === 2) {
+                  t.pass();
+                }
+              }
+            }
+          });
 
-          if (++matches === 2) {
-            t.pass();
-          }
+          readStream.on('end', () => {
+            leveldb.close();
+            t.end();
+          });
         }
-      }
+      }, 50);
     });
-
-    readStream.on('end', () => {
-      leveldb.close();
-      t.end();
-    });
-  }, 4000);
+  });
 });
