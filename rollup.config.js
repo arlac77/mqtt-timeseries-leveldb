b2289a26f6a56bd4944269f8da00c2a1235888f0
Markus Felten <Markus.Felten@gmx.de>
2018-09-08 21:58:18 +0200

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import cleanup from "rollup-plugin-cleanup";
+import pkg from "./package.json";
 
 export default {
   input: pkg.module,
   output: {
     file: pkg.main,
-    format: 'cjs',
+    format: "cjs",
     interop: false
   },
-  plugins: [],
-  external: ['mqtt']
+  plugins: [resolve(), commonjs(), cleanup()],
+  external: ["mqtt"]
 };
